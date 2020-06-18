4ff646cea0ac2023bca7bcf8292c9a9c82d3dc92
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-06-18 21:28:03 +0000

    Add `onHeaderAvailable` to `CustomRecyclerView`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/CustomRecyclerView.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/CustomRecyclerView.kt
@@ -3,13 +3,45 @@ package net.mullvad.mullvadvpn.ui.widget
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
+import android.view.View
+import kotlin.properties.Delegates.observable
 import net.mullvad.mullvadvpn.util.ListenableScrollableView
 
 class CustomRecyclerView : RecyclerView, ListenableScrollableView {
+    private val headerReadyCallback = object : OnLayoutChangeListener {
+        override fun onLayoutChange(
+            view: View,
+            left: Int,
+            top: Int,
+            right: Int,
+            bottom: Int,
+            oldLeft: Int,
+            oldTop: Int,
+            oldRight: Int,
+            oldBottom: Int
+        ) {
+            synchronized(this@CustomRecyclerView) {
+                headerAvailable = true
+                onHeaderAvailable?.invoke()
+                removeOnLayoutChangeListener(this)
+            }
+        }
+    }
+
+    private var headerAvailable = false
+
     override var horizontalScrollOffset = 0
     override var verticalScrollOffset = 0
 
     override var onScrollListener: ((Int, Int, Int, Int) -> Unit)? = null
+
+    var onHeaderAvailable by observable<(() -> Unit)?>(null) { _, _, listener ->
+        synchronized(this) {
+            if (headerAvailable) {
+                listener?.invoke()
+            }
+        }
+    }
 
     constructor(context: Context) : super(context) {}
 
@@ -17,6 +49,10 @@ class CustomRecyclerView : RecyclerView, ListenableScrollableView {
 
     constructor(context: Context, attributes: AttributeSet, defaultStyleAttribute: Int) :
         super(context, attributes, defaultStyleAttribute) {
+    }
+
+    init {
+        addOnLayoutChangeListener(headerReadyCallback)
     }
 
     override fun onScrolled(horizontalDelta: Int, verticalDelta: Int) {
